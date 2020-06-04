be3ec72cf955fe5c98184cdd664db280bf8d38cc
Behnam Nikkhah <bnikkhah22@gmail.com>
2020-06-03 21:08:57 -0600

    fix navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ import { AnchorLink } from 'gatsby-plugin-anchor-links'
 
 import './style.scss';
 import { Controller, Scene } from 'react-scrollmagic';
-import { Tween } from 'react-gsap'
+import { Tween, Timeline } from 'react-gsap'
 
 const Navbar = () => {
 	// const [open, setOpen] = useState(false)
@@ -31,38 +31,31 @@ const Navbar = () => {
 
 	return (
 		<Controller>
-			<Scene duration={600} triggerElement="#about">
-				{
-					(progress, event) => (
-						<Tween
-							to={{ y: -100 }}
-							ease="elastic.out"
-							paused
-							playState={
-								(event.type === 'enter' && event.scrollDirection === 'FORWARD') ? 'play' : 
-								(event.type === 'leave' && event.scrollDirection === 'REVERSE') ? 'reverse' : null
-							}
-						>
-							<nav className="menu" id="navbar">
-								<ul className="menu-list">
-									{
-										navItems.map((item) => (
-											<li key={item.title}>
-												<AnchorLink
-													to={item.link}
-													key={item.title}
-													title={item.title}
-												>
-													{item.title}
-												</AnchorLink>
-											</li>
-										))
-									}
-								</ul>
-							</nav>
-						</Tween>
-					)
-				}
+			<Scene duration={1300}>
+				<Timeline
+					target={
+						<nav className="menu" id="navbar">
+							<ul className="menu-list">
+								{
+									navItems.map((item) => (
+										<li key={item.title}>
+											<AnchorLink
+												to={item.link}
+												key={item.title}
+												title={item.title}
+											>
+												{item.title}
+											</AnchorLink>
+										</li>
+									))
+								}
+							</ul>
+						</nav>
+					}
+				>
+					<Tween from={{ x: 0 }} />
+					<Tween to={{ x: 100 }} />
+				</Timeline>
 			</Scene>
 		</Controller>
 	)
