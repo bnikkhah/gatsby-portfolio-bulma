d5dc60a4b5329f570f103294feff0796c002dc59
Behnam Nikkhah <bnikkhah22@gmail.com>
2021-01-04 21:07:58 -0700

    remove contact us due to bots

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => {
             <About />
             <Skills />
             <Projects />
-            <Contact />
+            {/* <Contact /> */}
         </Layout>
     )
 }
